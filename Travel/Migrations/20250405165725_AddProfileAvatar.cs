eb9c06494554e989ad059ca7f0cea08736d3cc90
Koha <khoaendy@gmail.com>
2025-04-06 22:54:32 +0700

    Update User Management:

---
+++ Travel/Migrations/20250405165725_AddProfileAvatar.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Travel.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfileAvatar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarPath",
+                table: "AspNetUsers",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "TourImage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TourId = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UploadDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TourImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TourImage_Tours_TourId",
+                        column: x => x.TourId,
+                        principalTable: "Tours",
+                        principalColumn: "TourID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TourImage_TourId",
+                table: "TourImage",
+                column: "TourId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TourImage");
+
+            migrationBuilder.DropColumn(
+                name: "AvatarPath",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ Travel/Models/TourImage.cs
@@ -0,0 +1,12 @@
+﻿namespace Travel.Models
+{
+    public class TourImage
+    {
+        public int Id { get; set; }
+        public int TourId { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime UploadDate { get; set; }
+        public Tour Tour { get; set; }
+
+    }
+}
